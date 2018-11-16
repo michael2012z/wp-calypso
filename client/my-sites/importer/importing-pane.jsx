9b7b6e63fd17db9a5be2f42cd5c3ea936ffca11c
spen <spen_@hotmail.co.uk>
2018-11-16 17:28:25 +0000

    Importers: Add checlist link to done state when no posts or pages are imported

--- client/my-sites/importer/importing-pane.jsx
+++ client/my-sites/importer/importing-pane.jsx
@@ -177,7 +177,15 @@ class ImportingPane extends React.PureComponent {
 			);
 		}
 
-		return translate( 'Import complete!' );
+		return translate(
+			'Import complete! Check out our handy {{a}}handy checklist{{/a}}' +
+				' to explore your settings and customizing options.',
+			{
+				components: {
+					a: checklistLink,
+				},
+			}
+		);
 	};
 
 	getImportMessage = numResources => {
